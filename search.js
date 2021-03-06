3c4074cc5573b807e2c67327d51ffafd81e3d0eb
mitsui <m.mihye1128@gmail.com>
2021-03-07 01:43:12 +0900

    fix(search)ユーザーデータ詳細 取得 Fixes #2

--- search.js
+++ search.js
@@ -33,94 +33,55 @@ const eventParticipationUrl = 'https://techtomo.connpass.com/event/204388/partic
 
   const userData = await page.evaluate(() => {
     const profileArea = document.getElementsByClassName('profile_header_area')[0];
-    const userMessage = profileArea.getElementsByTagName('p')[0];
-    return userMessage.innerText;
+    const profileMessage = profileArea.getElementsByTagName('p')[0].innerText;
+
+    const snsLinkArea = profileArea.getElementsByClassName('social_link')[0];
+    const SNS = Object.values(snsLinkArea.getElementsByTagName('a')).map((element) => {
+      const snsUrl = element.href;
+      if (snsUrl.match(/twitter.com/)) {
+        return {'Twitter': snsUrl}
+      } else if(snsUrl.match(/github.com/)) {
+        return {'GitHub': snsUrl}
+      }
+    });
+
+    const events = Object.values(document.getElementsByClassName('event_list')).map(event => {
+      const eventTitleElement = event.getElementsByClassName('event_title')[0].getElementsByTagName('a')[0];
+      const title = eventTitleElement.innerText;
+      const url = eventTitleElement.href;
+      const eventDateElements = event.getElementsByClassName('event_schedule_area')[0];
+      const year = eventDateElements.getElementsByClassName('year')[0].innerText;
+      const day = eventDateElements.getElementsByClassName('date')[0].innerText;
+      const date = year + '/' + day;
+      const amount = event.getElementsByClassName('amount')[0].getElementsByTagName('span')[0].innerText;
+      const groupName = event.getElementsByClassName('label_group')[0].getElementsByClassName('series_title')[0].innerText;
+      const groupUrl = event.getElementsByClassName('label_group')[0].getElementsByTagName('a')[0].href;
+      const owner = event.getElementsByClassName('event_owner')[0].getElementsByClassName('image_link')[0].innerText;
+      const ownerUrl = event.getElementsByClassName('event_owner')[0].getElementsByTagName('a')[0].href;
+      return {
+        title,
+        url,
+        date,
+        amount,
+        group: {
+          name: groupName || '',
+          groupUrl: groupUrl || '',
+        },
+        owner: {
+          owner,
+          ownerUrl
+        }
+      };
+    });
+
+    return {
+      profileMessage,
+      SNS,
+      events
+    };
   });
 
   console.log(userData);
-
-  // ユーザーページへ遷移
-  // let index = 0;
-  // promiseList = [];
-  // testUserUrlList.forEach(userUrl => {
-  //   console.log('test1')
-  //   promiseList.push((async (index) => {
-  //     console.log('test2')
-  //     const page = await browser.newPage();
-  //     page.setDefaultNavigationTimeout(30000);// default 3000 milliseconds, pass 0 to disable timeout
-  //     const response = await page.goto(userUrl);
-  //     await page.waitForNavigation(1000);// 1秒待つ
-
-  //     if (response.status() !== 200) {
-  //       return [];
-  //     }
-
-  //     console.log(index);
-
-  //     const result = await page.evaluate(async() => {
-  //       return await page.title();
-  //     });
-
-  //     console.log(result);
-
-  //     await page.close();
-
-  //     return result;
-  //   })(index));
-  //   index++;
-  // });
-  // let articleTitleList = [];
-  // await Promise.all(promiseList).then(valueList => {
-  //   valueList.forEach(value => {
-  //     articleTitleList = articleTitleList.concat(value);
-  //   });
-  // }).catch(reject => {
-  //   throw reject;
-  // });
-  // console.log(articleTitleList);
-  // (async function() {
-  //   console.log('test0');
-  //   for(let i = 0; i < testUserUrlList.length; i++) {
-  //     console.log('test1');
-  //     const page = await browser.newPage()
-  //     const res = await page.goto(testUserUrlList[i], {waitUntil: 'domcontentloaded'})
-  //     await page.waitForNavigation(5000);
-  //     console.log(res);
-  //     if (res.status() !== 200) return `${res.status()} ERROR`
-  //     await page.evaluate(async () => {
-  //       console.log('test');
-  //       const title = await page.title();
-  //       console.log(title);
-  //     })
-  //     // console.log(result)
-  //     await page.close()
-  //   }
-
-    // await Promise.all(testUserUrlList.map(async (userUrl) => {
-    //   await page.goto(userUrl, {
-    //     waitUntil: 'networkidle2'
-    //   });
-    //   await page.waitForNavigation(5000);
-    //   const title = await page.title();
-    //   console.log(title);
-    // }))
-  // })();
-  // (async () => {
-  //   for(let i; i < test.length; i++) {
-  //     await page.goto(test[i], {
-  //       waitUntil: 'domcontentloaded'
-  //     });
-  //     const title = await page.title();
-  //     console.log(title);
-  //     // const userName = title.replace('さんのプロフィール（申し込みイベント一覧） - connpass', '');
-  //     // console.log(userName);
-  //   }
-  // })();
-  // test.map(async(userUrl) => {
-  //   await page.goto(userUrl, {
-  //     waitUntil: 'domcontentloaded'
-  //   });
-  // })
 
   await browser.close();
 })()
